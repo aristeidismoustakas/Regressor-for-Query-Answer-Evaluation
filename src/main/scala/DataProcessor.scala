8bb2b8fe4ef0fc1e87a07c3ae57726d29e18718e
adamantmc <adamantiosminas@gmail.com>
2018-01-02 20:43:00 +0200

    Merged product descriptions with attributes, creating 2 DataFrames - one containing product_uid, search term and relevance and one containing product info

--- src/main/scala/DataProcessor.scala
+++ src/main/scala/DataProcessor.scala
@@ -1,17 +1,24 @@
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.collect_list
 
 object DataProcessor {
 
     def main(args: Array[String]): Unit = {
         val df_tup = read_data("data/train.csv", "data/attributes.csv", "data/product_descriptions.csv")
-
         df_tup._1.show(10)
         df_tup._2.show(10)
-        df_tup._3.show(10)
     }
 
-
-    def read_data(train_csv: String, attributes_csv: String, descriptions_csv: String): (DataFrame, DataFrame, DataFrame) = {
+    /***
+      * Reads data from CSV files and returns 2 DataFrames
+      * First DataFrame contains: (id, product_uid, search_term, relevance)
+      * Second DataFrame contains: (product_uid, product_description, product_attributes, product_title)
+      * @param train_csv train.csv
+      * @param attributes_csv attributes.csv
+      * @param descriptions_csv product_descriptions.csv
+      * @return Tuple with the 2 DataFrames
+      */
+    def read_data(train_csv: String, attributes_csv: String, descriptions_csv: String): (DataFrame, DataFrame) = {
         val spark = SparkSession
             .builder()
             .master("local")
@@ -19,14 +26,22 @@ object DataProcessor {
             .getOrCreate()
 
         val train_df = spark.read.format("csv").option("header", "true").csv(train_csv)
+
         val attributes_df = spark.read.format("csv").option("header", "true").csv(attributes_csv)
+        val attributes_df_grouped = attributes_df.groupBy(attributes_df.col("product_uid")).agg(collect_list(columnName = "value"))
         val descriptions_df = spark.read.format("csv").option("header", "true").csv(descriptions_csv)
 
-        (train_df, attributes_df, descriptions_df)
+        val train_df_without_product_names = train_df.select("id", "product_uid", "search_term", "relevance")
+        val train_df_product_names = train_df.select("product_uid", "product_title").dropDuplicates("product_uid")
+
+        val products = descriptions_df
+            .join(attributes_df_grouped, Seq("product_uid"), "outer")
+            .join(train_df_product_names, Seq("product_uid"), "outer")
+
+        (train_df_without_product_names, products.toDF("product_uid", "product_description", "product_attributes", "product_title"))
     }
 
-    def process_data(train_df: DataFrame, attributes_df: DataFrame, descriptions_df: DataFrame): Unit = {
-
+    def process_data(train_df: DataFrame, products_df: DataFrame): Unit = {
     }
 
 }
