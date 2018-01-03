82c79a3437aafc0f76137289f4090b7606a9fdda
adamantmc <adamantiosminas@gmail.com>
2018-01-03 22:47:02 +0200

    Some comments

--- src/main/scala/DataProcessor.scala
+++ src/main/scala/DataProcessor.scala
@@ -25,23 +25,31 @@ object DataProcessor {
             .appName("RelevancePrediction")
             .getOrCreate()
 
+        // Read train.csv
         val train_df = spark.read.format("csv").option("header", "true").csv(train_csv)
 
+        // Read attributes.csv, group by product and collect all attributes per product in a list
         val attributes_df = spark.read.format("csv").option("header", "true").csv(attributes_csv)
         val attributes_df_grouped = attributes_df.groupBy(attributes_df.col("product_uid")).agg(collect_list(columnName = "value"))
+
+        // Read product_descriptions.csv
         val descriptions_df = spark.read.format("csv").option("header", "true").csv(descriptions_csv)
 
+        // Separate product titles from training dataframe
         val train_df_without_product_names = train_df.select("id", "product_uid", "search_term", "relevance")
         val train_df_product_names = train_df.select("product_uid", "product_title").dropDuplicates("product_uid")
 
+        // Create a dataframe with all product info
         val products = descriptions_df
             .join(attributes_df_grouped, Seq("product_uid"), "outer")
             .join(train_df_product_names, Seq("product_uid"), "outer")
 
+        // Return
         (train_df_without_product_names, products.toDF("product_uid", "product_description", "product_attributes", "product_title"))
     }
 
     def process_data(train_df: DataFrame, products_df: DataFrame): Unit = {
     }
+
 
 }
