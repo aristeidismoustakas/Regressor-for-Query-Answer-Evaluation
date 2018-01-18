32f751fe17237d8af572910fb7b21fcf279b3f64
adamantmc <adamantiosminas@gmail.com>
2018-01-19 01:02:04 +0200

    Regressor trees ensemble

--- build.sbt
+++ build.sbt
@@ -9,8 +9,20 @@ val sparkVersion = "2.2.0"
 libraryDependencies ++= Seq(
     "org.apache.spark" %% "spark-core" % sparkVersion,
     "org.apache.spark" %% "spark-sql" % sparkVersion,
-    "org.apache.spark" %% "spark-mllib" % sparkVersion
+    "org.apache.spark" %% "spark-mllib" % sparkVersion,
     //"com.github.master" %% "spark-stemming" % sparkVersion
+
+    "org.scalanlp" %% "breeze" % "0.13.2",
+
+    // Native libraries are not included by default. add this if you want them (as of 0.7)
+    // Native libraries greatly improve performance, but increase jar sizes.
+    // It also packages various blas implementations, which have licenses that may or may not
+    // be compatible with the Apache License. No GPL code, as best I know.
+    "org.scalanlp" %% "breeze-natives" % "0.13.2",
+
+    // The visualization library is distributed separately as well.
+    // It depends on LGPL code
+    "org.scalanlp" %% "breeze-viz" % "0.13.2"
 )
 
 //libraryDependencies += "com.github.master" %% "spark-stemming" % "0.2.0"
--- src/main/scala/DataProcessor.scala
+++ src/main/scala/DataProcessor.scala
@@ -1,24 +1,66 @@
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.ml.feature.StopWordsRemover
 import org.apache.spark.ml.feature.HashingTF
 import org.apache.spark.ml.feature.IDF
 import org.apache.spark.ml.feature.Tokenizer
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.{DenseVector, SparseVector}
+import breeze.linalg.{DenseVector => BDV, SparseVector => BSV, Vector => BV}
+import breeze.linalg.functions.cosineDistance
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
-import org.apache.spark.ml.linalg.SparseVector
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.regression.RandomForestRegressor
 
 object DataProcessor {
 
     def main(args: Array[String]): Unit = {
+        val t0 = System.currentTimeMillis()
+
         Logger.getLogger("org").setLevel(Level.OFF)
         Logger.getLogger("akka").setLevel(Level.OFF)
 
-        val train = read_data("data/train.csv", "data/attributes.csv", "data/product_descriptions.csv")
-        train.show(10)
+        val toDouble = udf[Double, String]( _.toDouble)
 
-        val processed_df = process_data(train)
-        processed_df.show(10)
+        val train_rel_str = read_data("data/train.csv", "data/attributes.csv", "data/product_descriptions.csv").toDF("product_uid", "id", "product_title",
+            "search_term", "relevance", "product_attributes", "product_description")
+
+        val train = train_rel_str.withColumn("rel_num", toDouble(train_rel_str.col("relevance"))).drop("relevance").withColumnRenamed("rel_num", "relevance")
+//
+//        val test = read_data("data/test.csv", "data/attributes.csv", "data/product_descriptions.csv").toDF("product_uid", "id", "product_title",
+//            "search_term", "product_attributes", "product_description")
+
+        val processed_train_df = process_data(train)
+//        val processed_test_df = process_data(test)
+
+        processed_train_df.show(10)
+//        processed_test_df.show(10)
+
+        val split = processed_train_df.randomSplit(Array(0.7, 0.3), seed=42)
+
+        val training_set = split(0)
+        val validation_set = split(1)
+
+        val rf = new RandomForestRegressor()
+            .setLabelCol("relevance")
+            .setFeaturesCol("features")
+            .fit(training_set)
+
+        val predictions = rf.transform(validation_set)
+
+        val evaluator = new RegressionEvaluator()
+            .setLabelCol("relevance")
+            .setPredictionCol("prediction")
+            .setMetricName("rmse")
+
+        val rmse = evaluator.evaluate(predictions)
+
+        val t1 = System.currentTimeMillis()
+
+        println("Execution time: " + (t1-t0)/1000 + "s")
+        println("Root Mean Squared Error (RMSE) on test data = " + rmse)
+
     }
 
     /***
@@ -51,13 +93,19 @@ object DataProcessor {
             .join(descriptions_df, Seq("product_uid"), "left_outer")
             .na.fill("")
 
-        train.toDF("product_uid", "id", "product_title",
-            "search_term", "relevance", "product_attributes", "product_description")
+        train
     }
 
     def process_data(products: DataFrame): DataFrame = {
         val col_names = Seq("search_term", "product_description", "product_attributes", "product_title")
         var dfs = new Array[DataFrame](col_names.length + 1)
+
+        val udf_cos_sim = udf((vec1: DenseVector, vec2: DenseVector) => {
+            val vec_1_bdv = BDV(vec1.toDense.toArray)
+            val vec_2_bdv = BDV(vec2.toDense.toArray)
+
+            1 - cosineDistance(vec_1_bdv, vec_2_bdv)
+        })
 
         dfs(0) = products
         var count = 0
@@ -100,7 +148,17 @@ object DataProcessor {
         })
 
         // Return processed dataset
-        dfs(count)
+        val tfidf = dfs(count)
+        val sims = tfidf
+            .withColumn("search_term_title_cos_sim", udf_cos_sim(tfidf.col("search_term_tfidf"), tfidf.col("product_title_tfidf")))
+            .withColumn("search_term_desc_cos_sim", udf_cos_sim(tfidf.col("search_term_tfidf"), tfidf.col("product_description_tfidf")))
+            .withColumn("search_term_attr_cos_sim", udf_cos_sim(tfidf.col("search_term_tfidf"), tfidf.col("product_attributes_tfidf")))
+
+        val assembler = new VectorAssembler()
+            .setInputCols(Array("search_term_title_cos_sim", "search_term_desc_cos_sim", "search_term_attr_cos_sim"))
+            .setOutputCol("features")
+
+        assembler.transform(sims)
     }
 
 }
